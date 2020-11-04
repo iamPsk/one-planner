a3c275e1ec9bf25f00bf56ff4667ca97e7d6ea55
webHero <khonjelwayo@gmail.com>
2020-11-04 09:10:34 +0200

    init

--- src/app/dashboard/components/side-nav/side-nav.component.ts
+++ src/app/dashboard/components/side-nav/side-nav.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {  
+  faStickyNote, 
+  faBell, 
+  faArchive, 
+  faTrashAlt,
+  faPlay,
+  faLightbulb
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css']
 })
+
 export class SideNavComponent implements OnInit {
+  
+  faStickyNote = faStickyNote 
+  faBell = faBell
+  faArchive = faArchive
+  faTrashAlt = faTrashAlt
+  faPlay = faPlay
+  faLightbulb = faLightbulb
+  
+  noteList = ["apps", "strings", "birthday parties", "meeting"];
 
   constructor() { }
 
--- src/app/dashboard/components/top-nav/top-nav.component.ts
+++ src/app/dashboard/components/top-nav/top-nav.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars, faSearch, faHome, faScrewdriver, faArchive } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-top-nav',
+  selector: 'top-nav',
   templateUrl: './top-nav.component.html',
   styleUrls: ['./top-nav.component.css']
 })
+
 export class TopNavComponent implements OnInit {
+  faBars = faBars;
+  faSearch = faSearch
+  faHome = faHome
+  faScrewdriver = faScrewdriver
+  faArchive = faArchive
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
